d9259f80a8a279242ac84282e4f357556890d525
Jason Roberts <osjasonroberts@gmail.com>
2013-04-28 15:29:42 +0800

    Added warning (Obsolete) for simulated gateways
    If you build with a non DEBUG build and use the non-real store gateways
    you now get compiler warnings

--- src/InAppPurchaseToggle/NuGet/Content/windows8/InAppPurchaseToggleCode/CurrentAppSimulatorWindowsStoreGateway.cs
+++ src/InAppPurchaseToggle/NuGet/Content/windows8/InAppPurchaseToggleCode/CurrentAppSimulatorWindowsStoreGateway.cs
@@ -1,7 +1,11 @@
-﻿using Windows.ApplicationModel.Store;
+﻿using System;
+using Windows.ApplicationModel.Store;
 
 namespace InAppPurchaseToggle
 {
+#if DEBUG
+    [Obsolete("Be careful not to accidentally use this in the final app")]
+#endif
     public class CurrentAppSimulatorWindowsStoreGateway : IWindowsStoreGateway
     {
         public bool LookupActiveStatus(string inAppOfferName)
--- src/InAppPurchaseToggle/NuGet/Content/windows8/InAppPurchaseToggleCode/SimulatedNeverPurchasedGateway.cs
+++ src/InAppPurchaseToggle/NuGet/Content/windows8/InAppPurchaseToggleCode/SimulatedNeverPurchasedGateway.cs
@@ -1,5 +1,10 @@
-﻿namespace InAppPurchaseToggle
+﻿using System;
+
+namespace InAppPurchaseToggle
 {
+#if DEBUG
+    [Obsolete("Be careful not to accidentally use this in the final app")]
+#endif
     public class SimulatedNeverPurchasedGateway : IWindowsStoreGateway
     {
         public bool LookupActiveStatus(string inAppOfferName)
