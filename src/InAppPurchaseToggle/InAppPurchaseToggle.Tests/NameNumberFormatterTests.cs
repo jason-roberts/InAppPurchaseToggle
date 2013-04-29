eebc33bd636618de315e723ca349dafd03c76a4a
Jason Roberts <osjasonroberts@gmail.com>
2013-04-29 18:02:03 +0800

    Fixed dodgy commit
    NameNumberFormatterTests.cs missing

---
+++ src/InAppPurchaseToggle/InAppPurchaseToggle.Tests/NameNumberFormatterTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace InAppPurchaseToggle.Tests
+{
+    public class NameNumberFormatterTests
+    {
+        [Fact]
+        public void ShouldAppendInstanceNumberAfterUnderscore()
+        {
+            var sut = new NameNumberFormatter();
+
+            var inAppOfferFullName = sut.Combine("Coin", 34);
+
+            Assert.Equal("Coin34", inAppOfferFullName);
+        }
+    }
+}
