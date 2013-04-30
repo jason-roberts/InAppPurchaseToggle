6e1fd91a7759775c73e73bf8151b7d6312becf24
Jason Roberts <osjasonroberts@gmail.com>
2013-04-30 20:16:05 +0800

    Added Coin example to sample proj

---
+++ src/InAppPurchaseToggle/SampleApplication/Coin.cs
@@ -0,0 +1,33 @@
+﻿namespace SampleApplication
+{
+    public class Coin : InAppPurchaseToggle.RepeatPurchaseToggleBase
+    {
+        protected override int SetAvailableStoreInstances()
+        {
+            return 100;
+        }
+
+        protected override InAppPurchaseToggle.IRepeatPurchaseToggleNameInstanceFormatter SetNameInstanceFormatter()
+        {
+            return new InAppPurchaseToggle.NameNumberFormatter();            
+        }
+    }
+
+    class MyClass
+    {
+         void DemoCode()
+         {
+             var t = new Coin();
+
+             int totalInstancesAvailable = t.AvailableStoreInstances;
+
+             int nextUnpurchased = t.GetNextLowestUnpurchasedInstance();
+
+             int totalInstancesPurchasedSoFar = t.GetTotalPurchased();
+
+             bool userPurchasedAllInstances = t.IsAllPurchased();
+
+             bool isThirtySecondInstancePurchased = t.IsInstancePurchased(32);
+         }
+    }
+}
